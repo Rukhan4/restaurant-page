a96544b5b832f6a68aea7766c2ceaba8f74d2611
Surur Khan <surur.rkhan@gmail.com>
2022-06-20 16:40:53 -0400

    Loading for home and contact

--- src/website.js
+++ src/website.js
@@ -1,3 +1,6 @@
+import loadHome from "./home";
+import createContact from "./contact";
+
 function createHeader() {
     const header = document.createElement("header");
     header.classList.add("header");
