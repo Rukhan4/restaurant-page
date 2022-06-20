ff1263a77fcae64aec98fdd85e17a7aae9ae6d49
Surur Khan <surur.rkhan@gmail.com>
2022-06-20 16:40:34 -0400

    Created contact page

---
+++ src/contact.js
@@ -0,0 +1,22 @@
+function createContact() {
+    const contact = document.createElement("div");
+    contact.classList.add("contact");
+
+    const phone = document.createElement("p");
+    phone.textContent = "Phone: +1-555-555-5555";
+
+    const address = document.createElement("p");
+    address.textContent = "Address: In your dreams";
+
+    const map = document.createElement("img");
+    map.src = "./images/map.png";
+    map.alt = "map";
+
+    contact.appendChild(phone);
+    contact.appendChild(address);
+    contact.appendChild(map);
+
+    return contact;
+};
+
+export default createContact;
